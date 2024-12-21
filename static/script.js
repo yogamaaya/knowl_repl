a5cc97ed389a6eb57b36607f363333c45abc9266
yogamaaya <motadaka@yahoo.com>
2024-12-21 14:53:09 +0000

    Assistant checkpoint: Fix message formatting and mail link
    Assistant generated file changes:
    - static/script.js: Fix mail link and emoji spacing
    
    ---
    
    User prompt:
    
    please fix  currentPageMessages = [
            "Who is Knowl and How to Use?",
            `✨ Knowl is intented to be your fun partner to understand a text from a different perspective of your own ✨<br><br>
            🌕 Please check the current document source to get an idea of what prompts to give Knowl.You can update the source by clicking Change Text Source button, and retrying if the updating doesn't work.<br><br>
            🌖  Knowl is currently in beta and may have some bugs. Please report any bugs <a href=
    "mailto:asknowl.ai@gmail.com?&subject=Bug Report&body=Found a bug for Knowl? Please describe the bug and steps to reproduce it. Thank you!","target="_blank"> here </a>
            🌗 Whenever you paste new text, please ask <b>new and specific questions to get new answers.</b><br>
            Knowl retains all information of text corpora given to date.<br><br>
            🌘 You can try rephrasing the same question or command if a response doesn't please you!<br><br>
            🌑 Try to use keywords of the text, and reference the current context as much as possible.<br><br>
            <p>PS: Please be patient with Knowl as she thinks~ 🦉</p>`
        ]; to have correct mail redirect opening in new tab and ensuring a line break exists before each moon emoji

--- static/script.js
+++ static/script.js
@@ -329,14 +329,19 @@ window.addEventListener('load', async function() {
     currentPageMessages = [
         "Who is Knowl and How to Use?",
         `✨ Knowl is intented to be your fun partner to understand a text from a different perspective of your own ✨<br><br>
-        🌕 Please check the current document source to get an idea of what prompts to give Knowl.You can update the source by clicking Change Text Source button, and retrying if the updating doesn't work.<br><br>
-        🌖  Knowl is currently in beta and may have some bugs. Please report any bugs <a href=
-"mailto:asknowl.ai@gmail.com?&subject=Bug Report&body=Found a bug for Knowl? Please describe the bug and steps to reproduce it. Thank you!","target="_blank"> here </a>
-        🌗 Whenever you paste new text, please ask <b>new and specific questions to get new answers.</b><br>
-        Knowl retains all information of text corpora given to date.<br><br>
-        🌘 You can try rephrasing the same question or command if a response doesn't please you!<br><br>
-        🌑 Try to use keywords of the text, and reference the current context as much as possible.<br><br>
-        <p>PS: Please be patient with Knowl as she thinks~ 🦉</p>`
+
+🌕 Please check the current document source to get an idea of what prompts to give Knowl. You can update the source by clicking Change Text Source button, and retrying if the updating doesn't work.<br><br>
+
+🌖 Knowl is currently in beta and may have some bugs. Please report any bugs <a href="mailto:asknowl.ai@gmail.com?&subject=Bug Report&body=Found a bug for Knowl? Please describe the bug and steps to reproduce it. Thank you!" target="_blank" rel="noopener noreferrer">here</a><br><br>
+
+🌗 Whenever you paste new text, please ask <b>new and specific questions to get new answers.</b><br>
+Knowl retains all information of text corpora given to date.<br><br>
+
+🌘 You can try rephrasing the same question or command if a response doesn't please you!<br><br>
+
+🌑 Try to use keywords of the text, and reference the current context as much as possible.<br><br>
+
+<p>PS: Please be patient with Knowl as she thinks~ 🦉</p>`
     ];
     updateChat(currentPageMessages);
     await updateDocumentToast();
